8d324ec47c64f3e06229762d50f0829cb6635cde
Adrián Balboa Tomé <a25adrianbt@teis.intranet>
2025-09-23 20:15:23 +0200

    Ejercicio para comprobar si es divisible por N

---
+++ _4FindNumbersDivisibleByN.java
@@ -0,0 +1,36 @@
+package loopExercices;
+
+
+import java.util.Scanner;
+
+/**
+ * Muestra los M números divisibles por N
+ *
+ * Si M = 5 y N = 2 el resultado será algo como
+ * 1: 2
+ * 2: 4
+ */
+
+public class _4FindNumbersDivisibleByN {
+
+    public static void main(String[] args) {
+
+        final int M = 5;
+        final int N = 2;
+
+        for (int i =0;i < M ; i= i + N){
+
+            System.out.println(i);
+
+
+        }
+
+
+
+        
+
+
+
+
+    }
+}
